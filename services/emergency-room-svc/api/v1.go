791e6881c88174564b7e0cd04b7ea01258fe87ce
Max Baumann <max@bmn.dev>
2022-12-19 11:44:49 +0100

    feat(er-svc): add create er handler

--- services/emergency-room-svc/api/v1.go
+++ services/emergency-room-svc/api/v1.go
@@ -1,1 +1,24 @@
 package api
+
+import (
+	"common"
+	"emergency-room-svc/models"
+	"github.com/google/uuid"
+
+	daprcmn "github.com/dapr/go-sdk/service/common"
+)
+
+type CreateERRequestV1 struct {
+	models.EmergencyRoomBase
+	Departments []uuid.UUID
+}
+
+type GetSingleERResponseV1 struct {
+	ID uuid.UUID `json:"id"`
+	models.EmergencyRoomBase
+	Departments []models.DepartmentBase `json:"departments"`
+}
+
+func (response GetSingleERResponseV1) ToContent() (*daprcmn.Content, error) {
+	return common.ToContentJson(response, "types.helpwave.de/user-svc/GetSingleERResponseV1")
+}
