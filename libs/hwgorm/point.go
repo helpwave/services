f2160edff34db904a0725b40578fea4a95459b04
Max Baumann <max@bmn.dev>
2022-12-19 11:25:20 +0100

    feat(er-svc): add models and migrations

---
+++ libs/hwgorm/point.go
@@ -0,0 +1,57 @@
+package hwgorm
+
+import (
+	"database/sql/driver"
+	"errors"
+	"strconv"
+)
+
+// Thank you mkopriv (https://stackoverflow.com/a/68521736)
+
+type Point struct {
+	Lat  float64 `json:"lat" validate:"required"`
+	Long float64 `json:"long" validate:"required"`
+}
+
+// Value converts a Point into a []byte for the database driver to consume
+func (p Point) Value() (driver.Value, error) {
+	out := []byte{'('}
+	out = strconv.AppendFloat(out, p.Lat, 'f', -1, 64)
+	out = append(out, ',')
+	out = strconv.AppendFloat(out, p.Long, 'f', -1, 64)
+	out = append(out, ')')
+	return out, nil
+}
+
+// Scan converts a src (from the database) into a Point
+func (p *Point) Scan(src interface{}) (err error) {
+	var data []byte
+	switch src := src.(type) {
+	case []byte:
+		data = src
+	case string:
+		data = []byte(src)
+	case nil:
+		return nil
+	default:
+		return errors.New("(*Point).Scan: unsupported data type")
+	}
+
+	if len(data) == 0 {
+		return nil
+	}
+
+	data = data[1 : len(data)-1] // drop the surrounding parentheses
+	for i := 0; i < len(data); i++ {
+		if data[i] == ',' {
+			if p.Lat, err = strconv.ParseFloat(string(data[:i]), 64); err != nil {
+				return err
+			}
+			if p.Long, err = strconv.ParseFloat(string(data[i+1:]), 64); err != nil {
+				return err
+			}
+			break
+		}
+	}
+	return nil
+}
---
+++ services/emergency-room-svc/migrations/000001_init.up.sql
@@ -0,0 +1,25 @@
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+
+CREATE TABLE IF NOT EXISTS emergency_rooms (
+    id uuid PRIMARY KEY DEFAULT uuid_generate_v4(),
+    name text NOT NULL,
+    location point,
+    displayable_address text NOT NULL,
+    is_open BOOL NOT NULL DEFAULT true,
+    utilization smallint NOT NULL DEFAULT 1,
+    CHECK (utilization >= 1 AND utilization <=5)
+);
+
+CREATE TABLE IF NOT EXISTS departments (
+    id uuid PRIMARY KEY DEFAULT uuid_generate_v4(),
+    name text NOT NULL UNIQUE
+);
+
+CREATE TABLE IF NOT EXISTS rooms_have_departments (
+    emergency_room_id UUID NOT NULL,
+    department_id UUID NOT NULL,
+    FOREIGN KEY (emergency_room_id)
+        REFERENCES emergency_rooms(id),
+    FOREIGN KEY (department_id)
+        REFERENCES departments(id)
+);
