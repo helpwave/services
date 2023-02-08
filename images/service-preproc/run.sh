f5f5cac430cc88c5a3c817b3604363ccad16ca2c
Max Baumann <max@bmn.dev>
2023-02-08 21:06:09 +0100

    make run.sh more flexible

--- images/service-preproc/run.sh
+++ images/service-preproc/run.sh
@@ -1,7 +1,11 @@
-cd api &&
-protoc --go_out=. \
-		--go_opt=paths=source_relative \
-		--go-grpc_out=. \
-		--go-grpc_opt=paths=source_relative \
-		*.proto &&
-protoc-go-inject-tag -input="*.pb.go"
+[ -d "api" ] && cd api || exit 0
+
+if ls ./api/*.proto &>/dev/null
+then
+	protoc --go_out=. \
+			--go_opt=paths=source_relative \
+			--go-grpc_out=. \
+			--go-grpc_opt=paths=source_relative \
+			*.proto &&
+	protoc-go-inject-tag -input="*.pb.go"
+fi
