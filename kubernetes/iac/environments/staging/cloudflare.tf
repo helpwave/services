06c189b0792398939d12343af26cca5bb1d03560
Max Baumann <max@bmn.dev>
2023-02-10 23:10:36 +0100

    feat(iac): sso subdomains (#67)

--- kubernetes/iac/environments/staging/cloudflare.tf
+++ kubernetes/iac/environments/staging/cloudflare.tf
@@ -17,6 +17,10 @@ resource "cloudflare_zone" "helpwave-de" {
 }
 */
 
+//
+// API
+//
+
 resource "cloudflare_record" "api-helpwave-de" {
   zone_id = var.cf_zone_id
   name = "api"
@@ -39,6 +43,28 @@ resource "cloudflare_record" "staging-api-helpwave-de" {
   zone_id = var.cf_zone_id
   name = "staging-api"
   value = cloudflare_record.api-helpwave-de.hostname
+  type = "CNAME"
+  comment = "Managed through Terraform"
+  proxied = true
+}
+
+//
+// Keycloak
+//
+
+resource "cloudflare_record" "sso-helpwave-de" {
+  zone_id = var.cf_zone_id
+  name = "sso"
+  value = cloudflare_record.api-helpwave-de.hostname
+  type = "CNAME"
+  comment = "Managed through Terraform"
+  proxied = true
+}
+
+resource "cloudflare_record" "staging-sso-helpwave-de" {
+  zone_id = var.cf_zone_id
+  name = "staging-sso"
+  value = cloudflare_record.sso-helpwave-de.hostname
   type = "CNAME"
   comment = "Managed through Terraform"
   proxied = true
