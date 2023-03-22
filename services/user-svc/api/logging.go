e87a5505939d0532ed0c7755069e9f77097c9a96
Max Baumann <max@bmn.dev>
2023-03-22 11:27:07 +0100

    feat: add user update routes

--- services/user-svc/api/logging.go
+++ services/user-svc/api/logging.go
@@ -7,3 +7,14 @@ func (r *CreateUserRequest) LoggableFields() map[string]interface{} {
 	m["password"] = logging.OmitAll(r.Password)
 	return m
 }
+
+func (r *UpdateUserRequest) LoggableFields() map[string]interface{} {
+	m := logging.StructToMap(r)
+	if r.Email != nil {
+		m["email"] = logging.OmitAll(*r.Email)
+	}
+	if r.Password != nil {
+		m["password"] = logging.OmitAll(*r.Password)
+	}
+	return m
+}
