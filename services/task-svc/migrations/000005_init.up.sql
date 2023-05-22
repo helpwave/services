cb57166f4ece3eab5cfdbf35403df95a95a12fd3
DasProffi <felix.thape@gmail.com>
2023-05-22 15:14:32 +0200

    fix: make migration use alter table

--- services/task-svc/migrations/000005_init.up.sql
+++ services/task-svc/migrations/000005_init.up.sql
@@ -8,12 +8,5 @@ CREATE TABLE IF NOT EXISTS subtasks (
 		ON DELETE CASCADE
 );
 
-CREATE TABLE IF NOT EXISTS tasks (
-	 id uuid PRIMARY KEY DEFAULT uuid_generate_v4(),
-	 name text NOT NULL,
-	 description text  NOT NULL DEFAULT '',
-	 status integer NOT NULL,
-	 assigned_user_id uuid DEFAULT uuid_nil(),
-	 patient_id uuid NOT NULL,
-	 public bool NOT NULL DEFAULT FALSE
-);
+ALTER TABLE tasks
+	 ADD public bool NOT NULL DEFAULT FALSE;
