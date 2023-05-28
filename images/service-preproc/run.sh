f312f18ee7475af72f3644d937dc7523e1885bb7
Max Schäfer <40192999+MaxSchaefer@users.noreply.github.com>
2023-05-29 00:06:22 +0200

    chore: run gofmt during make proto (#168)

--- images/service-preproc/run.sh
+++ images/service-preproc/run.sh
@@ -9,4 +9,5 @@ buf $CMD $@
 
 if [ "$CMD" = "generate" ]; then
 	protoc-go-inject-tag -input="gen/go/proto/*/*/*/*.pb.go" # Note this only matches exactly three dirs, as go's glob does not support '**' (https://github.com/golang/go/issues/11862)
+	gofmt -w gen/go
 fi
