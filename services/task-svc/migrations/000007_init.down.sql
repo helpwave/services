c1a7c0d1437609f4b773f8b05f572cabcf2293a6
Max Schäfer <max-schaefer-lg@gmx.de>
2023-06-03 01:23:12 +0200

    fix: remove rooms migrations

--- services/task-svc/migrations/000007_init.down.sql
+++ services/task-svc/migrations/000007_init.down.sql
@@ -1,6 +1,3 @@
 ALTER TABLE patients
 	DROP COLUMN notes,
 	DROP COLUMN bed_id;
-
-ALTER TABLE rooms
-	DROP COLUMN ward_id;
--- services/task-svc/migrations/000007_init.up.sql
+++ services/task-svc/migrations/000007_init.up.sql
@@ -3,8 +3,3 @@ ALTER TABLE patients
 	ADD COLUMN bed_id uuid,
 	ADD FOREIGN KEY (bed_id)
 		REFERENCES beds(id);
-
-ALTER TABLE rooms
-	ADD COLUMN ward_id uuid,
-	ADD FOREIGN KEY (ward_id)
-		REFERENCES wards(id);
