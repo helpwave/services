f2f11ecc44e37122c07007443b8d8164dde4a9de
Max Schäfer <max-schaefer-lg@gmx.de>
2023-06-09 00:11:34 +0200

    fix: use invitation_state

--- services/user-svc/migrations/000002_init.up.sql
+++ services/user-svc/migrations/000002_init.up.sql
@@ -4,7 +4,7 @@ CREATE TABLE IF NOT EXISTS invitations  (
 	id uuid PRIMARY KEY DEFAULT uuid_generate_v4(),
 	email varchar(255) NOT NULL,
 	organization_id uuid NOT NULL,
-	state state NOT NULL,
+	state invitation_state NOT NULL,
 	FOREIGN KEY (organization_id)
 		REFERENCES organizations(id)
 	 	ON DELETE CASCADE
