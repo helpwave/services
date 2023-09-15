74cb981d017c200921309135f98aaa7c319cbc33
Max Baumann <max@bmn.dev>
2023-09-15 11:44:22 +0200

    add well-known imports

---
+++ gen/ts/google/protobuf/timestamp_pb.d.ts
@@ -0,0 +1,26 @@
+import * as jspb from 'google-protobuf'
+
+
+
+export class Timestamp extends jspb.Message {
+  getSeconds(): number;
+  setSeconds(value: number): Timestamp;
+
+  getNanos(): number;
+  setNanos(value: number): Timestamp;
+
+  serializeBinary(): Uint8Array;
+  toObject(includeInstance?: boolean): Timestamp.AsObject;
+  static toObject(includeInstance: boolean, msg: Timestamp): Timestamp.AsObject;
+  static serializeBinaryToWriter(message: Timestamp, writer: jspb.BinaryWriter): void;
+  static deserializeBinary(bytes: Uint8Array): Timestamp;
+  static deserializeBinaryFromReader(message: Timestamp, reader: jspb.BinaryReader): Timestamp;
+}
+
+export namespace Timestamp {
+  export type AsObject = {
+    seconds: number,
+    nanos: number,
+  }
+}
+
