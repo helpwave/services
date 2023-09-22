e665b7f5706a26d75d71319d28842f2246602a76
Paul-B98 <115164840+Paul-B98@users.noreply.github.com>
2023-09-22 16:20:55 +0200

    add grpc

--- services/impulse_svc/grpc_service/service.py
+++ services/impulse_svc/grpc_service/service.py
@@ -1,11 +1,11 @@
-from grpc_service import controller_pb2
-from grpc_service import controller_pb2_grpc
+from grpc_service import impulse_svc_pb2_grpc
+from grpc_service import impulse_svc_pb2
 
 
-class MYServicer(controller_pb2_grpc.ControllerServicer):
+class MYServicer(impulse_svc_pb2_grpc.ControllerServicer):
 
     def Get(self, request, context):
-        return controller_pb2.GetResponse(value="Test")
+        return impulse_svc_pb2.GetResponse(value="Test")
 
 def grpc_hook(server):
-    controller_pb2_grpc.add_ControllerServicer_to_server(MYServicer(), server)
+    impulse_svc_pb2_grpc.add_ControllerServicer_to_server(MYServicer(), server)
