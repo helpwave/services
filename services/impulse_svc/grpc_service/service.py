ebe58305f7495190f9f1e6b254b14d77d69f1586
Friedjof <friedjof@noweck.info>
2023-09-22 19:40:58 +0200

    fix: logging

--- services/impulse_svc/grpc_service/service.py
+++ services/impulse_svc/grpc_service/service.py
@@ -1,11 +1,14 @@
 from grpc_service import impulse_svc_pb2_grpc
 from grpc_service import impulse_svc_pb2
 
+from grpc_service.models import Challenge, UserChallenge, User
+
 
 class Servicer(impulse_svc_pb2_grpc.ImpulseService):
 
     def CreateUser(self, request, context):
-        pass
+        for msg in request:
+            print(msg)
 
     def UpdateUser(self, request, context):
         pass
