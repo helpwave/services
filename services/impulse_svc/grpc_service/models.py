c4a7af28f9880a06ef50d167dcb5de9b9dbf2d29
Friedjof <friedjof@noweck.info>
2023-09-22 16:42:57 +0200

    add: models

--- services/impulse_svc/grpc_service/models.py
+++ services/impulse_svc/grpc_service/models.py
@@ -1,3 +1,40 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
-# Create your models here.
+
+class Challenge(models.Model):
+	class ChallengeTypes(models.TextChoices):
+		DAILY = 'daily', _('Täglich')
+		QUEST = 'quest', _('Mission')
+
+	class ChallengeCategories(models.TextChoices):
+		FOOD = 'food', _('Ernährung')
+		FITNESS = 'fitness', _('Fitness')
+		MENTAL = 'mental', _('Mentale Gesundheit')
+
+	id: str = models.UUIDField(primary_key=True)
+	title: str = models.CharField(max_length=50)
+	description: str = models.CharField(max_length=200)
+
+	start_datetime: str = models.DateTimeField()
+	end_datetime: str = models.DateTimeField()
+
+	type: str = models.CharField(max_length=10, choices=ChallengeTypes.choices)
+	category: str = models.CharField(max_length=10, choices=ChallengeCategories.choices)
+
+	points: int = models.IntegerField()
+
+
+class UserChallenge(models.Model):
+	score: int = models.IntegerField()
+	done: bool = models.BooleanField()
+
+
+class User(models.Model):
+	id: str = models.UUIDField(primary_key=True)
+	username: str = models.CharField(max_length=50)
+	sex: str = models.CharField(max_length=1)
+	age: int = models.IntegerField()
+	pal: float = models.FloatField()
+
+	challenges: list = models.ManyToManyField(Challenge, through=UserChallenge)
