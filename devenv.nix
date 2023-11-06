ed76c04d3b7a245f01e0e0da9707f4cef7b9306b
Max Schäfer <40192999+MaxSchaefer@users.noreply.github.com>
2023-11-06 20:18:33 +0100

    chore: add devenv (#478)
    * chore: add devenv
    
    * chore: using air for every service
    
    * chore: update readme
    
    * chore: document ports, fix proto buf
    
    * fix: lint nix

---
+++ devenv.nix
@@ -0,0 +1,24 @@
+{ pkgs, buildPackages, fetchurl, ... }:
+
+let
+  protoc-go-inject-tag = import ./nix/protoc-go-inject-tag.nix { inherit pkgs; };
+  listEntries = path:
+    map (name: path + "/${name}") (builtins.attrNames (builtins.readDir path));
+in
+{
+  imports = listEntries ./nix/devenv;
+
+  packages = with pkgs; [
+    air
+    buf
+    dapr-cli
+    flyctl
+    gh
+    git
+    go-migrate
+    nixpkgs-fmt
+  ] ++ [
+    # self build packages
+    protoc-go-inject-tag
+  ];
+}
