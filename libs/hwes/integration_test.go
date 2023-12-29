4803a8eaab5353f6a786916f456797c33e8b64a0
Max Schäfer <max-schaefer-lg@gmx.de>
2023-12-29 23:21:49 +0100

    test: add integration test for hwes

---
+++ libs/hwes/integration_test.go
@@ -0,0 +1,149 @@
+package hwes_test
+
+import (
+	"context"
+	"github.com/google/uuid"
+	"hwes"
+	"hwes/test"
+	"testing"
+)
+
+type UserModel struct {
+	ID       uuid.UUID
+	Username string
+}
+
+func NewUserModel() *UserModel {
+	return &UserModel{}
+}
+
+const UserAggregateType = "user"
+
+type UserAggregate struct {
+	*hwes.AggregateBase
+	User *UserModel
+}
+
+func (a *UserAggregate) onUserCreated(evt hwes.Event) error {
+	var payload UserCreatedEvent
+	if err := evt.GetJsonData(&payload); err != nil {
+		return err
+	}
+
+	id, err := uuid.Parse(payload.ID)
+	if err != nil {
+		return err
+	}
+
+	a.User.ID = id
+	a.User.Username = payload.Username
+
+	return nil
+}
+
+func (a *UserAggregate) onUsernameUpdated(evt hwes.Event) error {
+	var payload UsernameUpdatedEvent
+	if err := evt.GetJsonData(&payload); err != nil {
+		return err
+	}
+
+	a.User.Username = payload.Username
+
+	return nil
+}
+
+func (a *UserAggregate) When(evt hwes.Event) error {
+	switch evt.EventType {
+	case UserCreated:
+		return a.onUserCreated(evt)
+	case UsernameUpdated:
+		return a.onUsernameUpdated(evt)
+	}
+	return nil
+}
+
+func NewUserAggregate(id uuid.UUID) *UserAggregate {
+	aggregate := &UserAggregate{User: NewUserModel()}
+	aggregate.AggregateBase = hwes.NewAggregateBase(UserAggregateType, id, aggregate.When)
+	aggregate.User.ID = id
+	return aggregate
+}
+
+const UserCreated = "USER_CREATED"
+
+type UserCreatedEvent struct {
+	ID       string
+	Username string
+}
+
+func NewUserCreatedEvent(a hwes.Aggregate, id uuid.UUID, username string) (hwes.Event, error) {
+	payload := UserCreatedEvent{
+		ID:       id.String(),
+		Username: username,
+	}
+	return hwes.NewBaseEventWithPayload(a, UserCreated, &payload)
+}
+
+const UsernameUpdated = "USER_UPDATED"
+
+type UsernameUpdatedEvent struct {
+	PreviousUsername string
+	Username         string
+}
+
+func NewUsernameUpdatedEvent(a hwes.Aggregate, previousUsername, username string) (hwes.Event, error) {
+	payload := UsernameUpdatedEvent{
+		PreviousUsername: previousUsername,
+		Username:         username,
+	}
+	return hwes.NewBaseEventWithPayload(a, UsernameUpdated, &payload)
+}
+
+func TestIntegration(t *testing.T) {
+	ctx := context.Background()
+	aggregateStore := test.NewAggregateStore()
+
+	id := uuid.New()
+	userAggregate := NewUserAggregate(id)
+
+	// Apply USER_CREATED event
+
+	userCreatedEvent, err := NewUserCreatedEvent(userAggregate, id, "testine.test")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if err := userAggregate.Apply(userCreatedEvent); err != nil {
+		t.Fatal(err)
+	}
+
+	// Apply USERNAME_UPDATED event
+
+	usernameUpdatedEvent, err := NewUsernameUpdatedEvent(userAggregate, userAggregate.User.Username, "testine.test")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if err := userAggregate.Apply(usernameUpdatedEvent); err != nil {
+		t.Fatal(err)
+	}
+
+	if err := aggregateStore.Save(ctx, userAggregate); err != nil {
+		t.Fatal(err)
+	}
+
+	// Now load
+
+	loadedUserAggregate := NewUserAggregate(id)
+	if err := aggregateStore.Load(ctx, loadedUserAggregate); err != nil {
+		t.Fatal(err)
+	}
+
+	if loadedUserAggregate.User.ID != id {
+		t.Fatal("invalid id")
+	}
+
+	if loadedUserAggregate.User.Username != "testine.test" {
+		t.Fatal("invalid username")
+	}
+}
