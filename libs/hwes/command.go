837b0bc53e72c238fa897137870b35eed1fbcce3
Max Schäfer <max-schaefer-lg@gmx.de>
2024-01-13 14:42:54 +0100

    add example for CommandBase

--- libs/hwes/command.go
+++ libs/hwes/command.go
@@ -5,6 +5,17 @@ import "github.com/google/uuid"
 // CommandBase acts as a container for the targeted AggregateID
 // A concrete command in our CQRS pattern will call NewCommandBase
 // in there initialization.
+//
+// Example:
+//
+//	type AssignTaskCommand struct {
+//		hwes.CommandBase
+//		UserID uuid.UUID
+//	}
+//
+//	func NewAssignTaskCommand(taskID, userID uuid.UUID) *AssignTaskCommand {
+//		return &AssignTaskCommand{CommandBase: hwes.NewCommandBase(taskID), UserID: userID}
+//	}
 type CommandBase struct {
 	aggregateID uuid.UUID
 }
