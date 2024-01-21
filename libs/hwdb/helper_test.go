3a02724cb9bc0a896d3cf563b7898d2be6b460d2
Max Baumann <max@bmn.dev>
2024-01-21 12:07:44 +0100

    add helper_test.go

---
+++ libs/hwdb/helper_test.go
@@ -0,0 +1,30 @@
+package hwdb_test
+
+import (
+	"google.golang.org/protobuf/types/known/timestamppb"
+	"hwdb"
+	"testing"
+)
+
+func TestPbToTimestamp(t *testing.T) {
+	t.Run("src = nil", func(t *testing.T) {
+		if hwdb.PbToTimestamp(nil).Valid {
+			t.Error()
+		}
+	})
+
+	t.Run("src not nil", func(t *testing.T) {
+		src := timestamppb.Timestamp{
+			Seconds: 0,
+			Nanos:   0,
+		}
+		res := hwdb.PbToTimestamp(&src)
+		if !res.Valid {
+			t.Error("returned ts with Valid = False")
+		}
+
+		if src.AsTime().UTC() != res.Time {
+			t.Error("time was not constructed the way it should be")
+		}
+	})
+}
