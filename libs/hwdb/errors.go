a8f55841d6da1471955974d608eec0fe84ca44dc
Max Baumann <max@bmn.dev>
2024-02-12 13:46:10 +0100

    errors.go

---
+++ libs/hwdb/errors.go
@@ -0,0 +1,140 @@
+package hwdb
+
+import (
+	"context"
+	"errors"
+	"github.com/golang/protobuf/proto"
+	"github.com/jackc/pgerrcode"
+	"github.com/jackc/pgx/v5/pgconn"
+	"github.com/rs/zerolog"
+	zlog "github.com/rs/zerolog/log"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+)
+
+package hwdb
+
+import (
+	"errors"
+    "github.com/jackc/pgerrcode"
+    "github.com/jackc/pgx/v5/pgconn"
+    "google.golang.org/grpc/codes"
+    "google.golang.org/grpc/status"
+)
+
+// TODO: move to hwutil
+func newStatusError(ctx context.Context, code codes.Code, msg string, details ...proto.Message) error {
+	statusNoDetails := status.New(code, msg)
+
+	s, err := statusNoDetails.WithDetails(details...)
+	if err != nil {
+		zlog.Ctx(ctx).Error().Err(err).Msg("could not add details to status error")
+		return statusNoDetails.Err()
+	}
+	return s.Err()
+}
+
+// Error expects a database error and returns a processed Error, also does logging
+func Error(ctx context.Context, err error) error {
+	log := zlog.Ctx(ctx)
+	if err == nil {
+		return nil
+	}
+	var pgError *pgconn.PgError
+	if !errors.As(err, &pgError) {
+		log.Warn().
+			Err(err).
+			Msg("hwdb.Error() called with unrecognized error type")
+		return err // just return it
+	}
+
+	newLogger := log.With().
+		Dict(
+			"pgErr",
+			zerolog.Dict().
+				Str("severity", pgError.Severity).
+				Str("code", pgError.Code).
+				Str("message", pgError.Message).
+				Str("detail", pgError.Detail).
+				Str("hint", pgError.Hint).
+				Str("internalQuery", pgError.InternalQuery).
+				Str("where", pgError.Where).
+				Str("schemaName", pgError.SchemaName).
+				Str("tableName", pgError.TableName).
+				Str("columnName", pgError.ColumnName).
+				Str("dataTypeName", pgError.DataTypeName).
+				Str("constraintName", pgError.ConstraintName).
+				Str("file", pgError.File).
+				Int32("line", pgError.Line).
+				Str("routine", pgError.Routine).
+        ).
+		Logger()
+	ctx = log.WithContext(ctx)
+	log = &newLogger
+
+	errCode := pgError.Code
+
+	// return nil
+	if pgerrcode.IsSuccessfulCompletion(errCode) {
+		return nil
+	}
+	if pgerrcode.IsWarning(errCode) {
+		log.Warn().
+			Msg("database warning")
+		return nil
+	}
+
+	// user made a mistake
+	if pgerrcode.IsIntegrityConstraintViolation(errCode) ||
+		pgerrcode.IsWithCheckOptionViolation(errCode) ||
+		pgerrcode.IsNoData(errCode) {
+		log.Info().Send()
+		return newStatusError(ctx, codes.InvalidArgument, "database error: " + pgError.Message) // TODO: details
+	}
+
+	// acts of god, we can only apologize
+	if pgerrcode.IsConnectionException(errCode) ||
+		pgerrcode.IsInsufficientResources(errCode) ||
+		pgerrcode.IsOperatorIntervention(errCode) ||
+		pgerrcode.IsSystemError(errCode) ||
+		pgerrcode.IsInternalError(errCode) ||
+		pgerrcode.IsProgramLimitExceeded(errCode) ||
+		pgerrcode.IsConfigurationFileError(errCode) {
+		log.Error().Msg("severe database issue! likely requires immediate action!")
+		return newStatusError(ctx, codes.Internal, "Could not perform action due to database issues") // TODO: details
+	}
+
+	// we made a mistake
+	if pgerrcode.IsFeatureNotSupported(errCode) ||
+		pgerrcode.IsInvalidTransactionInitiation(errCode) ||
+		pgerrcode.IsCardinalityViolation(errCode) ||
+		pgerrcode.IsDataException(errCode) ||
+		pgerrcode.IsInvalidTransactionState(errCode) ||
+		pgerrcode.IsInvalidSQLStatementName(errCode) ||
+		pgerrcode.IsTriggeredDataChangeViolation(errCode) ||
+		pgerrcode.IsInvalidAuthorizationSpecification(errCode) ||
+		pgerrcode.IsInvalidTransactionTermination(errCode) ||
+		pgerrcode.IsSQLRoutineException(errCode) ||
+		pgerrcode.IsInvalidCursorName(errCode) ||
+		pgerrcode.IsExternalRoutineException(errCode) ||
+		pgerrcode.IsSavepointException(errCode) ||
+		pgerrcode.IsInvalidCatalogName(errCode) ||
+		pgerrcode.IsInvalidSchemaName(errCode) ||
+		pgerrcode.IsTransactionRollback(errCode) ||
+		pgerrcode.IsSyntaxErrororAccessRuleViolation(errCode) ||
+		pgerrcode.IsObjectNotInPrerequisiteState(errCode) ||
+		pgerrcode.IsForeignDataWrapperError(errCode) ||
+		pgerrcode.IsSQLStatementNotYetComplete(errCode) ||
+		pgerrcode.IsTriggeredActionException(errCode) ||
+		pgerrcode.IsLocatorException(errCode) ||
+		pgerrcode.IsInvalidGrantor(errCode) ||
+		pgerrcode.IsInvalidRoleSpecification(errCode) ||
+		pgerrcode.IsDiagnosticsException(errCode) ||
+		pgerrcode.IsCaseNotFound(errCode) ||
+		pgerrcode.IsDependentPrivilegeDescriptorsStillExist(errCode) ||
+		pgerrcode.IsSnapshotFailure(errCode) {
+		return newStatusError(ctx, codes.Internal, "database error: " + pgError.Message) // TODO: details
+	}
+	log.Error().Msg("unrecognized database error has occurred")
+	return newStatusError(ctx, codes.Internal, "database error: " + pgError.Message) // TODO: details
+}
--- libs/hwdb/go.mod
+++ libs/hwdb/go.mod
@@ -9,11 +9,13 @@ replace (
 )
 
 require (
+	github.com/jackc/pgerrcode v0.0.0-20220416144525-469b46aa5efa
 	github.com/jackc/pgx/v5 v5.5.0
 	github.com/rs/zerolog v1.31.0
 	github.com/vgarvardt/pgx-google-uuid/v5 v5.0.0
 	go.opentelemetry.io/otel v1.21.0
 	go.opentelemetry.io/otel/trace v1.21.0
+	google.golang.org/grpc v1.61.0
 	google.golang.org/protobuf v1.31.0
 	hwutil v0.0.0
 	telemetry v0.0.0
@@ -28,6 +30,7 @@ require (
 	github.com/go-playground/locales v0.14.1 // indirect
 	github.com/go-playground/universal-translator v0.18.1 // indirect
 	github.com/go-playground/validator/v10 v10.16.0 // indirect
+	github.com/golang/protobuf v1.5.3 // indirect
 	github.com/google/uuid v1.4.0 // indirect
 	github.com/jackc/pgpassfile v1.0.0 // indirect
 	github.com/jackc/pgservicefile v0.0.0-20221227161230-091c0ba34f0a // indirect
@@ -39,7 +42,8 @@ require (
 	go.opentelemetry.io/otel/metric v1.21.0 // indirect
 	golang.org/x/crypto v0.15.0 // indirect
 	golang.org/x/net v0.18.0 // indirect
-	golang.org/x/sync v0.3.0 // indirect
+	golang.org/x/sync v0.5.0 // indirect
 	golang.org/x/sys v0.14.0 // indirect
 	golang.org/x/text v0.14.0 // indirect
+	google.golang.org/genproto/googleapis/rpc v0.0.0-20231106174013-bbf56f31fb17 // indirect
 )
