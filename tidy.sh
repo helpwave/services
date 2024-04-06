171de5da1d9fce0ae7a206f47d5b3b06d54cefcd
Max Baumann <max@bmn.dev>
2024-04-06 20:05:30 +0200

    indent

--- tidy.sh
+++ tidy.sh
@@ -2,7 +2,7 @@
 
 N=3
 for i in `seq $N`; do
-echo "== Run $i of $N =="
-find -type d -name gen -prune -o -name "go.mod" -exec sh -c 'cd $(dirname {}) && pwd && go mod tidy' \;
+	echo "== Run $i of $N =="
+	find -type d -name gen -prune -o -name "go.mod" -exec sh -c 'cd $(dirname {}) && pwd && go mod tidy' \;
 done
 
