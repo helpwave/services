509dbb03fa1b3bdc55151a167090f4c7789eb341
Max Baumann <max@bmn.dev>
2024-09-23 11:11:48 +0200

    GetBeds and GetBedsByRoom tests

--- services/tasks-svc/stories/BedCRUD_test.go
+++ services/tasks-svc/stories/BedCRUD_test.go
@@ -5,6 +5,7 @@ import (
 	pb "gen/services/tasks_svc/v1"
 	"github.com/stretchr/testify/assert"
 	"hwutil"
+	"strconv"
 	"testing"
 	"time"
 )
@@ -143,4 +144,67 @@ func TestGetBedByPatient(t *testing.T) {
 
 	assert.Equal(t, roomId, res.Room.Id)
 	assert.Equal(t, roomConsistency, res.Room.Consistency)
+}
+
+func TestGetBeds(t *testing.T) {
+	bedClient := bedServiceClient()
+	ctx := context.Background()
+
+	suffixMatrix := [][]string{
+		{"1 A", "1 B"}, // Room 1
+		{"2 A"},        // Room 2
+		{},             // Room 3
+	}
+
+	bedRoomMap := make(map[string]string)        // map bedID to its roomID
+	roomBedsMap := make(map[string][]string)     // map roomID to its bedIDs
+	bedConsistencyMap := make(map[string]string) // map bedID to its consistency
+
+	var room1Id string // for part 2
+
+	for i, bedSfxs := range suffixMatrix {
+		roomSuffix := strconv.Itoa(i + 1)
+		_, roomId, _ := prepareRoom(t, ctx, roomSuffix)
+		if i == 0 {
+			room1Id = roomId
+		}
+		roomBedsMap[roomId] = make([]string, 0)
+		for _, bedSuffix := range bedSfxs {
+			bedId, bedConsistency := prepareBed(t, ctx, roomId, bedSuffix)
+			bedRoomMap[bedId] = roomId
+			bedConsistencyMap[bedId] = bedConsistency
+			roomBedsMap[roomId] = append(roomBedsMap[roomId], bedId)
+		}
+	}
+
+	allBeds, err := bedClient.GetBeds(ctx, &pb.GetBedsRequest{})
+	assert.NoError(t, err, "could not get all beds")
+
+	assert.GreaterOrEqual(t, len(allBeds.Beds), 3) // other beds might exist from other tests
+
+	found := 0
+	for _, bed := range allBeds.Beds {
+		assert.NotEqual(t, "", bed.Id)
+		if bedRoomMap[bed.Id] == "" {
+			continue
+		}
+		found++
+		assert.Equal(t, bedRoomMap[bed.Id], bed.RoomId)
+		assert.Equal(t, bedConsistencyMap[bed.Id], bed.Consistency)
+	}
+
+	assert.Equal(t, 3, found)
+
+	// Part 2: GetBedsByRoom
+
+	res, err := bedClient.GetBedsByRoom(ctx, &pb.GetBedsByRoomRequest{RoomId: room1Id})
+	assert.NoError(t, err, "could not get beds for room 1")
+
+	assert.Len(t, res.Beds, 2)
+
+	bedIds := hwutil.Map(res.Beds, func(bed *pb.GetBedsByRoomResponse_Bed) string {
+		return bed.Id
+	})
+
+	assert.Subset(t, roomBedsMap[room1Id], bedIds)
 }
