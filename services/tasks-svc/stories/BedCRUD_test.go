d0636638a01f89d5e3b1c2618f7c15cb0b50c23e
Max Baumann <max@bmn.dev>
2024-09-23 10:14:46 +0200

    TestGetBedByPatient

--- services/tasks-svc/stories/BedCRUD_test.go
+++ services/tasks-svc/stories/BedCRUD_test.go
@@ -6,9 +6,10 @@ import (
 	"github.com/stretchr/testify/assert"
 	"hwutil"
 	"testing"
+	"time"
 )
 
-func prepareRoom(t *testing.T, ctx context.Context, suffix string) (wardID, roomID string) {
+func prepareRoom(t *testing.T, ctx context.Context, suffix string) (wardID, roomID, roomConsistency string) {
 	wardRes, err := wardServiceClient().CreateWard(ctx, &pb.CreateWardRequest{
 		Name: t.Name() + " ward " + suffix,
 	})
@@ -18,7 +19,26 @@ func prepareRoom(t *testing.T, ctx context.Context, suffix string) (wardID, room
 		WardId: wardRes.Id,
 	})
 	assert.NoError(t, err, "prepareRoom failed: could not create room", suffix)
-	return wardRes.Id, roomRes.Id
+	return wardRes.Id, roomRes.Id, roomRes.Consistency
+}
+
+func prepareBed(t *testing.T, ctx context.Context, roomId, suffix string) (bedID, consistency string) {
+	createRes, err := bedServiceClient().CreateBed(ctx, &pb.CreateBedRequest{
+		RoomId: roomId,
+		Name:   t.Name() + " bed " + suffix,
+	})
+	assert.NoError(t, err, "prepareBed: could not create bed", suffix)
+	return createRes.Id, createRes.Consistency
+}
+
+func preparePatient(t *testing.T, ctx context.Context) (patientID string) {
+	res, err := patientServiceClient().CreatePatient(ctx, &pb.CreatePatientRequest{
+		HumanReadableIdentifier: t.Name() + " Patient",
+		Notes:                   hwutil.PtrTo("A patient for test " + t.Name()),
+	})
+	assert.NoError(t, err, "preparePatient: could not create patient")
+	time.Sleep(time.Second)
+	return res.Id
 }
 
 // TestCreateUpdateGetBed:
@@ -29,14 +49,14 @@ func TestCreateUpdateGetBed(t *testing.T) {
 	bedClient := bedServiceClient()
 
 	// first, prepare room
-	_, roomId := prepareRoom(t, ctx, "1")
+	_, roomId, _ := prepareRoom(t, ctx, "1")
 
 	//
 	// create new bed
 	//
 	createReq := &pb.CreateBedRequest{
 		RoomId: roomId,
-		Name:   t.Name() + " room",
+		Name:   t.Name() + " bed",
 	}
 	createRes, err := bedClient.CreateBed(ctx, createReq)
 
@@ -60,7 +80,7 @@ func TestCreateUpdateGetBed(t *testing.T) {
 	//
 
 	// prepare new room
-	_, roomID2 := prepareRoom(t, ctx, "2")
+	_, roomID2, _ := prepareRoom(t, ctx, "2")
 
 	updateReq := &pb.UpdateBedRequest{
 		Id:          bedID,
@@ -84,4 +104,43 @@ func TestCreateUpdateGetBed(t *testing.T) {
 	assert.Equal(t, *updateReq.Name, getBedRes.Name)
 	assert.Equal(t, updateRes.Consistency, getBedRes.Consistency)
 
+}
+
+func TestGetBedByPatient(t *testing.T) {
+	ctx := context.Background()
+
+	// first, prepare room
+	_, roomId, roomConsistency := prepareRoom(t, ctx, "")
+
+	// creating two beds
+	unrelatedBedID, _ := prepareBed(t, ctx, roomId, "unrelated")
+	patientsBedID, bedConsistency := prepareBed(t, ctx, roomId, "patient")
+
+	// create a patient
+	patientID := preparePatient(t, ctx)
+
+	// assign bed to patient
+	_, err := patientServiceClient().AssignBed(ctx, &pb.AssignBedRequest{
+		Id:          patientID,
+		BedId:       patientsBedID,
+		Consistency: nil,
+	})
+	assert.NoError(t, err, "could not assign bed to patient")
+	time.Sleep(time.Second)
+
+	// GetBedByPatient
+	res, err := bedServiceClient().GetBedByPatient(ctx, &pb.GetBedByPatientRequest{
+		PatientId: patientID,
+	})
+	assert.NoError(t, err, "could not GetBedByPatient")
+
+	assert.NotNil(t, res.Bed)
+	assert.NotNil(t, res.Room)
+
+	assert.NotEqual(t, unrelatedBedID, res.Bed.Id)
+	assert.Equal(t, patientsBedID, res.Bed.Id)
+	assert.Equal(t, bedConsistency, res.Bed.Consistency)
+
+	assert.Equal(t, roomId, res.Room.Id)
+	assert.Equal(t, roomConsistency, res.Room.Consistency)
 }
