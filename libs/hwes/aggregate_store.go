f9b3922945b4037e77d2ee5e876c5c3fe53b0dde
Max Baumann <max@bmn.dev>
2024-09-27 17:37:20 +0200

    Update libs/hwes/aggregate_store.go
    Co-authored-by: Max Schäfer <40192999+MaxSchaefer@users.noreply.github.com>

--- libs/hwes/aggregate_store.go
+++ libs/hwes/aggregate_store.go
@@ -9,7 +9,7 @@ type AggregateStore interface {
 	// Load populates the aggregate to the recent version of the persisted events
 	Load(ctx context.Context, aggregate Aggregate) error
 
-	// Save persists all uncommitted events of the aggregate, returns consistency
+	// Save persists all uncommitted events of the aggregate, returns consistency token
 	Save(ctx context.Context, aggregate Aggregate) (uint64, error)
 
 	// Exists checks if the aggregate exists in the data store
