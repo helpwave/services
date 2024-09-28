6d64723809f49014ff328fe647b051b587785b4f
Max Baumann <max@bmn.dev>
2024-09-28 09:00:05 +0200

    Update services/tasks-svc/stories/RoomCRUD_test.go
    Co-authored-by: Max Schäfer <40192999+MaxSchaefer@users.noreply.github.com>

--- services/tasks-svc/stories/RoomCRUD_test.go
+++ services/tasks-svc/stories/RoomCRUD_test.go
@@ -185,7 +185,7 @@ func TestGetRoomOverviewsByWard(t *testing.T) {
 		PatientId:     patient1Id,
 		InitialStatus: hwutil.PtrTo(pb.TaskStatus_TASK_STATUS_DONE),
 	})
-	assert.NoError(t, err, "could create task for patient 1")
+	assert.NoError(t, err, "could not create task for patient 1")
 	_, err = taskClient.CreateTask(ctx, &pb.CreateTaskRequest{
 		Name:          t.Name() + " Patient 1 Task 4",
 		PatientId:     patient1Id,
