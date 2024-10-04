58f31bf6a43fd6cef4dad6c7b0e3083fa85c805b
PaulKalho <kalhornpaul@gmail.com>
2024-10-04 17:36:52 +0200

    added getTaskTemplate

--- services/tasks-svc/repos/task_template_repo/task_template_repo.sql.go
+++ services/tasks-svc/repos/task_template_repo/task_template_repo.sql.go
@@ -153,6 +153,55 @@ func (q *Queries) GetAllTaskTemplatesWithSubTasks(ctx context.Context, arg GetAl
 	return items, nil
 }
 
+const getTaskTemplateWithSubtaskByID = `-- name: GetTaskTemplateWithSubtaskByID :many
+SELECT
+	task_templates.id, task_templates.name, task_templates.description, task_templates.ward_id, task_templates.created_by, task_templates.consistency,
+	task_template_subtasks.id as sub_task_id,
+	task_template_subtasks.name as sub_task_name
+FROM
+	task_templates
+LEFT JOIN
+	task_template_subtasks
+ON
+	task_template_subtasks.task_template_id = task_templates.id
+WHERE task_templates.id = $1
+`
+
+type GetTaskTemplateWithSubtaskByIDRow struct {
+	TaskTemplate TaskTemplate
+	SubTaskID    uuid.NullUUID
+	SubTaskName  *string
+}
+
+func (q *Queries) GetTaskTemplateWithSubtaskByID(ctx context.Context, id uuid.UUID) ([]GetTaskTemplateWithSubtaskByIDRow, error) {
+	rows, err := q.db.Query(ctx, getTaskTemplateWithSubtaskByID, id)
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+	items := []GetTaskTemplateWithSubtaskByIDRow{}
+	for rows.Next() {
+		var i GetTaskTemplateWithSubtaskByIDRow
+		if err := rows.Scan(
+			&i.TaskTemplate.ID,
+			&i.TaskTemplate.Name,
+			&i.TaskTemplate.Description,
+			&i.TaskTemplate.WardID,
+			&i.TaskTemplate.CreatedBy,
+			&i.TaskTemplate.Consistency,
+			&i.SubTaskID,
+			&i.SubTaskName,
+		); err != nil {
+			return nil, err
+		}
+		items = append(items, i)
+	}
+	if err := rows.Err(); err != nil {
+		return nil, err
+	}
+	return items, nil
+}
+
 const updateSubtask = `-- name: UpdateSubtask :one
 UPDATE task_template_subtasks ttst
 SET	name = coalesce($1, name)
