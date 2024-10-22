c1a53059946d6ef9c5b74235de5fc88a960c7b57
Max Baumann <max@bmn.dev>
2024-10-22 15:17:41 +0200

    still works

--- services/property-svc/stories/GetAttachedPropertyValuesAlwaysInclude_test.go
+++ services/property-svc/stories/GetAttachedPropertyValuesAlwaysInclude_test.go
@@ -9,55 +9,76 @@ import (
 	"testing"
 )
 
-func TestGetAttachedPropertyValuesAlwaysInclude(t *testing.T) {
+func TestGetAttachedPropertyValuesAlwaysIncludeBasicValues(t *testing.T) {
 	ctx := context.Background()
 	propertyClient := propertyServiceClient()
 	propertyValueClient := propertyValueServiceClient()
 	propertyViewClient := propertyViewServiceClient()
 
-	patientID := uuid.New().String()
+	subjectTypes := []pb.SubjectType{
+		pb.SubjectType_SUBJECT_TYPE_PATIENT,
+		pb.SubjectType_SUBJECT_TYPE_TASK,
+	}
 
-	// Create a property
-	property, err := propertyClient.CreateProperty(ctx, &pb.CreatePropertyRequest{
-		SubjectType: pb.SubjectType_SUBJECT_TYPE_PATIENT,
-		FieldType:   pb.FieldType_FIELD_TYPE_NUMBER,
-		Name:        t.Name(),
-		Description: nil,
-	})
-	assert.NoError(t, err)
-	hwtesting.WaitForProjectionsToSettle()
+	fieldTypes := []pb.FieldType{
+		pb.FieldType_FIELD_TYPE_TEXT,
+		pb.FieldType_FIELD_TYPE_NUMBER,
+		pb.FieldType_FIELD_TYPE_CHECKBOX,
+		pb.FieldType_FIELD_TYPE_DATE,
+		pb.FieldType_FIELD_TYPE_DATE_TIME,
+		pb.FieldType_FIELD_TYPE_SELECT,
+		pb.FieldType_FIELD_TYPE_MULTI_SELECT,
+	}
 
-	// Don't create a property value
+	for _, subjectType := range subjectTypes {
+		for _, fieldType := range fieldTypes {
+			t.Run(t.Name()+"_"+subjectType.String()+"_"+fieldType.String(), func(t *testing.T) {
+				subjectID := uuid.New().String()
 
-	// Add property to the always_include list of an arbitrary subject
-	_, err = propertyViewClient.UpdatePropertyViewRule(ctx, &pb.UpdatePropertyViewRuleRequest{
-		FilterUpdate: &pb.FilterUpdate{
-			AppendToAlwaysInclude: []string{property.PropertyId},
-		},
-		Matcher: &pb.UpdatePropertyViewRuleRequest_PatientMatcher{
-			PatientMatcher: &pb.PatientPropertyMatcher{
-				WardId:    nil,
-				PatientId: &patientID,
-			},
-		},
-	})
-	assert.NoError(t, err)
-	hwtesting.WaitForProjectionsToSettle()
+				// Create a property
+				property, err := propertyClient.CreateProperty(ctx, &pb.CreatePropertyRequest{
+					SubjectType: subjectType,
+					FieldType:   fieldType,
+					Name:        t.Name(),
+					Description: nil,
+				})
+				assert.NoError(t, err)
+				hwtesting.WaitForProjectionsToSettle()
 
-	// Call GetAttachedPropertyValues for this arbitrary patient or task
+				// Don't create a property value
 
-	res, err := propertyValueClient.GetAttachedPropertyValues(ctx, &pb.GetAttachedPropertyValuesRequest{
-		Matcher: &pb.GetAttachedPropertyValuesRequest_PatientMatcher{
-			PatientMatcher: &pb.PatientPropertyMatcher{
-				WardId:    nil,
-				PatientId: &patientID,
-			},
-		},
-	})
-	assert.NoError(t, err)
+				// Add property to the always_include list of an arbitrary subject
+				_, err = propertyViewClient.UpdatePropertyViewRule(ctx, &pb.UpdatePropertyViewRuleRequest{
+					FilterUpdate: &pb.FilterUpdate{
+						AppendToAlwaysInclude: []string{property.PropertyId},
+					},
+					Matcher: &pb.UpdatePropertyViewRuleRequest_PatientMatcher{
+						PatientMatcher: &pb.PatientPropertyMatcher{
+							WardId:    nil,
+							PatientId: &subjectID,
+						},
+					},
+				})
+				assert.NoError(t, err)
+				hwtesting.WaitForProjectionsToSettle()
 
-	// Property shows up
-	assert.Len(t, res.Values, 1)
-	assert.Equal(t, res.Values[0].PropertyId, property.PropertyId)
+				// Call GetAttachedPropertyValues for this arbitrary patient or task
+
+				res, err := propertyValueClient.GetAttachedPropertyValues(ctx, &pb.GetAttachedPropertyValuesRequest{
+					Matcher: &pb.GetAttachedPropertyValuesRequest_PatientMatcher{
+						PatientMatcher: &pb.PatientPropertyMatcher{
+							WardId:    nil,
+							PatientId: &subjectID,
+						},
+					},
+				})
+				assert.NoError(t, err)
+
+				// Property shows up
+				assert.Len(t, res.Values, 1)
+				assert.Equal(t, res.Values[0].PropertyId, property.PropertyId)
+			})
+		}
+	}
 
 }
