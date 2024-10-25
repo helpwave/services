700dcfb1727478fd4a4bd70f8964f34e81a94247
Max Baumann <max@bmn.dev>
2024-10-25 16:35:14 +0200

    add most ward permissions

---
+++ services/tasks-svc/internal/ward/perm/permission.go
@@ -0,0 +1,28 @@
+package perm
+
+import (
+	"hwauthz"
+
+	"github.com/google/uuid"
+)
+
+// Types
+
+type Ward uuid.UUID
+
+func (t Ward) Type() string { return "ward" }
+func (t Ward) ID() string   { return uuid.UUID(t).String() }
+
+// Direct Relations
+
+const WardOrganization hwauthz.Relation = "organization"
+
+// Permissions
+
+const OrganizationCanUserCreateWard hwauthz.Permission = "create_ward"
+
+const (
+	WardCanUserGet    hwauthz.Permission = "get"
+	WardCanUserUpdate hwauthz.Permission = "update"
+	WardCanUserDelete hwauthz.Permission = "delete"
+)
