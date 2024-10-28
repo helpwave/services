48bf5470819b96e1c086ee4f8ff97c3b39f17ff9
Max Baumann <max@bmn.dev>
2024-10-28 17:07:33 +0100

    fix userorg

--- libs/hwauthz/commonPerm/userOrg.go
+++ libs/hwauthz/commonPerm/userOrg.go
@@ -12,12 +12,9 @@ type User uuid.UUID
 func (t User) Type() string { return "user" }
 func (t User) ID() string   { return uuid.UUID(t).String() }
 
-func UserFromCtx(ctx context.Context) (User, error) {
-	userID, err := auth.GetUserID(ctx)
-	if err != nil {
-		return User{}, err
-	}
-	return User(userID), nil
+func UserFromCtx(ctx context.Context) User {
+	userID := auth.MustGetUserID(ctx)
+	return User(userID)
 }
 
 type Organization uuid.UUID
@@ -25,10 +22,7 @@ type Organization uuid.UUID
 func (p Organization) Type() string { return "organization" }
 func (p Organization) ID() string   { return uuid.UUID(p).String() }
 
-func OrganizationFromCtx(ctx context.Context) (Organization, error) {
-	organizationID, err := auth.GetOrganizationID(ctx)
-	if err != nil {
-		return Organization{}, err
-	}
-	return Organization(organizationID), nil
+func OrganizationFromCtx(ctx context.Context) Organization {
+	organizationID := auth.MustGetOrganizationID(ctx)
+	return Organization(organizationID)
 }
