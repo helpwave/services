89e717792e743f16635be66f1fef8eaaa049986a
Max Baumann <max@bmn.dev>
2024-11-18 15:05:19 +0100

    document GenericObject

--- libs/hwauthz/commonPerm/generic.go
+++ libs/hwauthz/commonPerm/generic.go
@@ -2,6 +2,9 @@ package commonPerm
 
 import "hwauthz"
 
+// GenericObject implements hwauthz.Object and can be used for ad-hoc Objects
+// Use this only if it is not feasible to write a proper hwauthz.Object-implementing type
+// For example for tests, or if the type is only used once or so
 type GenericObject struct {
 	Typ hwauthz.ObjectType
 	Id  string
